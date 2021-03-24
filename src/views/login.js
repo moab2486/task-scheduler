63012554e1257e5f9b7094c88114dc8c80e4f4f3
Abdulrasak <abdulrasak.abdulkadir@datamaxregistrars.com>
2021-03-24 17:47:30 +0100

    ui login

--- src/views/login.js
+++ src/views/login.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 function Login(){
     return(
-        <div className="container">
+        <div className="login-container">
+            <div className="centered">
+                <h3>Let's sign you in.</h3>
+                <p>
+                    Welcome back. <br />
+                    You've been missed!
+                </p>
+            </div>
+
+
             <div className="input-wrapper">
                 <label htmlFor="" className="input-label">Username</label>
                 <div className="input-field">
@@ -10,7 +19,7 @@ function Login(){
                 </div>
             </div>
 
-            <div className="input-wrapper">
+            <div className="input-wrapper mt">
                 <label htmlFor="" className="input-label">Password</label>
                 <div className="input-field">
                     <input type="password" name="" id="" className="form-control"/>
@@ -21,16 +30,27 @@ function Login(){
                 <input type="checkbox" name="" id="" className="input-checklist"/> Remember me
             </div>
 
-            <div className="submit-wrapper">
-                <button className="btn-submit-button">Login</button>
+            <div className="centered mt">
+                <button className="btn-sm">Login</button>
             </div>
 
-            <div className="sign-alt">
+            <div className="mt">
+                <a href="/">sign up</a>
+                <a href="/" className="float-right">Forgot password?</a>
+            </div>
+
+            <div className="centered mt">
                <hr/> OR <hr />
             </div>
 
-            <div className="submit-wrapper">
-                <button className="btn-submit-button">continue with google</button>
+            <div className="input-wrapper mt">
+                <button className="btn-xl">continue with google</button>
+                <button className="btn-xl float-right">continue with facebook</button>
+            </div>
+
+            <div className="centered mt">
+                <p>Courtesy MoaB@2020</p>
+                <p>Designed by Abdulkadir</p>
             </div>
         </div>
     )
